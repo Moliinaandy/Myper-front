96f0ae126b4c4992eedab46f6df379d272d71c8e
Andrea Molina <molinaandy110@gmail.com>
2026-02-07 11:22:10 -0500

    [FIX] : correccion de configariones y agregar las funcionalidades del app vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+import HomeView from './views/HomeView.vue'
+import DashboardView from './views/DashboardView.vue'
+import BlogView from './views/BlogView.vue' 
+import SolutionsView from './views/SolutionsView.vue'
+import ResourcesView from './views/ResourcesView.vue'
+import LoginView from './views/LoginView.vue' 
+import ContactView from './views/ContactoView.vue'
+
+const currentView = ref('home')
+const isAuthenticated = ref(false)
+const navigateTo = (viewName) => {
+  if (viewName === 'dashboard' && !isAuthenticated.value) {
+    currentView.value = 'login'
+  } else {
+    currentView.value = viewName
+  }
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+const onLoginSuccess = () => {
+  isAuthenticated.value = true
+  currentView.value = 'dashboard'
+}
+
+const logout = () => {
+  isAuthenticated.value = false
+  currentView.value = 'home' 
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen flex flex-col font-sans text-slate-800 bg-white">
+    
+   <AppHeader 
+        v-if="currentView !== 'login'" 
+        :isAuthenticated="isAuthenticated" 
+        @navigate="navigateTo" 
+        @logout="logout"
+    />
+
+    <main class="flex-grow">
+      <Transition name="fade" mode="out-in">
+        <HomeView v-if="currentView === 'home'" />
+        <BlogView v-else-if="currentView === 'blog'" />
+        <SolutionsView v-else-if="currentView === 'solutions'" />
+        <ResourcesView v-else-if="currentView === 'resources'" />
+        <LoginView v-else-if="currentView === 'login'" @success="onLoginSuccess" />
+        <DashboardView v-else-if="currentView === 'dashboard'" />
+        <ContactView v-else-if="currentView === 'contact'" />
+      </Transition>
+    </main>
+
+    <AppFooter v-if="currentView !== 'login'" />
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
